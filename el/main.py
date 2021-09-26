3d26736ad9ee2788605a2c4803751b671ee58aa1
Juan Francisco <contrerasjf.0@hotmail.com>
2021-09-25 21:18:59 -0500

    feataure(REPL): Implement the firts version of REPEL
    * Create the repl file.
    * Create the main file as entry point.
    * update the _read_identifier methodo to validate the number inside the identifiers.
    
    Resolve: C-11

---
+++ el/main.py
@@ -0,0 +1,13 @@
+
+from lpp.repl import start_repl
+
+
+def main() -> None:
+    print('Bienvenido al Lenguaje de Programación Platzi.')
+    print('Escribe un oración para comenzar.')
+
+    start_repl()
+
+
+if __name__ == '__main__':
+    main()
