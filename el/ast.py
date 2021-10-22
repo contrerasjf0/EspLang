13888f9a66665aed8a22b3ae10ab94a778c5463e
Juan Francisco <contrerasjf.0@hotmail.com>
2021-10-21 21:49:08 -0500

    testing(parser): Implement base tests to test expressions
    * The expressionStatement class was defined in ast script.
    
    Resolve: C-22

--- el/ast.py
+++ el/ast.py
@@ -76,11 +76,21 @@ class LetStatement(Statement):
 class ReturnStatement(Statement):
 
     def __init__(self,
-                 token: Token,
-                 return_value: Optional[Expression] = None) -> None:
+                token: Token,
+                return_value: Optional[Expression] = None) -> None:
         super().__init__(token)
         self.return_value = return_value
 
     def __str__(self) -> str:
         return f'{self.token_literal()} {str(self.return_value)};'
 
+class ExpressionStatement(Statement):
+
+    def __init__(self,
+                token: Token,
+                expression: Optional[Expression] = None) -> None:
+        super().__init__(token)
+        self.expression = expression
+
+    def __str__(self) -> str:
+        return str(self.expression)
